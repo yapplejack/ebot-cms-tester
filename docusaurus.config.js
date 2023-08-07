ade3a46591cd0a3dc27e1394e9a792c5780db38e
Jack Ferguson <52545566+yapplejack@users.noreply.github.com>
2023-08-07 13:43:48 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,12 +15,12 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ebot/',
+  baseUrl: '/ebot-cms-tester/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'yapplejack', // Usually your GitHub org/user name.
-  projectName: 'ebot', // Usually your repo name.
+  projectName: 'ebot-cms-tester', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
